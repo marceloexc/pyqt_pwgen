fbd8f1305c142d1fd69ac6304d6a9e99973f1f26
marceloexc <marcelomendez0exc@gmail.com>
2021-01-09 23:44:36 -0600

    stage3

---
+++ pwgenerator.py
@@ -0,0 +1,41 @@
+import string
+import random
+from PyQt5 import QtCore, QtGui, uic, QtWidgets
+from PyQt5.QtWidgets import QMainWindow
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        uic.loadUi('MainWindow.ui', self)
+        self.show()
+        self.horizontalSlider.setValue(25)
+        self.horizontalSlider.valueChanged.connect(self.changeLength)
+        self.dict = {
+            'upper' : string.ascii_lowercase,
+            'lower': string.ascii_uppercase,
+            'digits': string.digits,
+            'special': string.punctuation
+        } #remove this 
+        print(self.checkBox.isChecked())
+        variationList = []
+
+
+    def changeLength(self):
+        self.label_6.clear()
+        self.label_6.setText(str(self.horizontalSlider.value()))
+        self.amount = self.horizontalSlider.value()
+        self.pw = ''
+        if self.checkBox.isChecked():
+            variationList.append(string.ascii_lowercase)
+        for i in range(self.amount):
+            key=random.choice(list(self.dict))
+            self.pw += str(random.choice(self.dict[key]))
+        self.plainTextEdit.clear()
+        self.plainTextEdit.setPlainText(self.pw)
+
+
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = MainWindow()
+    app.exec_()
